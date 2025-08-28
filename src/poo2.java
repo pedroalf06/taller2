4b6fb75dbe57253f36344aa385a3f85841ba7996
ALUMNO# <ALUMNO#@ALUMNO-08>
2025-08-28 10:16:39 -0500

    primer commit

---
+++ src/poo2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class poo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+int costokWh = 32500, consumoskWh = 1600,multipl;
+        multipl = costokWh * consumoskWh;
+        System.out.println("multipl ="+ multipl);
+        
+        int costototalkWh = 52000000;
+        double iva = 0.16, divic;
+        divic = costototalkWh / iva;
+        System.out.println("El consumo total es="+ divic);
+
+    }
+    
+}
