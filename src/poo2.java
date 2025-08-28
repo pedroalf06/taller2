593392ad580c14d56f5f10fdc798b33ab71c9b74
pedroalf06 <38568867+pedroalf06@users.noreply.github.com>
2025-08-28 11:14:40 -0500

    Update poo2. java
    corrección carga dos

--- src/poo2.java
+++ src/poo2.java
@@ -19,11 +19,12 @@ int costokWh = 32500, consumoskWh = 1600,multipl;
         multipl = costokWh * consumoskWh;
         System.out.println("multipl ="+ multipl);
         
-        int costototalkWh = 52000000;
+        //int costototalkWh = 52000000;
         double iva = 0.16, divic;
-        divic = costototalkWh / iva;
-        System.out.println("El consumo total es="+ divic);
+        divic = multipl * iva;
+        System.out.println("El consumo total es="+ (divic + multipl));
 
     }
     
 }
+
